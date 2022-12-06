e168fe5cc1ea73ec7e7d52fc90de5e1c1b87b057
licetullmann <licetrosales@gmail.com>
2022-12-06 22:00:30 +0100

    Add test getOrder

--- src/main/java/com/github/licetrosales/orderdbweblu/controller/ShopController.java
+++ src/main/java/com/github/licetrosales/orderdbweblu/controller/ShopController.java
@@ -1,6 +1,9 @@
 package com.github.licetrosales.orderdbweblu.controller;
 
+import com.github.licetrosales.orderdbweblu.model.Order;
+import com.github.licetrosales.orderdbweblu.model.Product;
 import com.github.licetrosales.orderdbweblu.service.ShopService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +16,27 @@ public class ShopController {
 
     // Constructor
     // Wir wollen keinen leeren Konstruktor (Dependency injection)
+    // Dependency Injection = Wofür genau?
+    /* Gibt Veantwortung an Spring ab
+     + weniger Code schreiben
+
+     Wir verwenden keine leeren Konstrukktoren
+     -> Spring verknüft unsere Abhängigkeiten automatisch
+     -> Testen wird einfacher
+        */
+
     public ShopController(ShopService shopService) {
         this.shopService = shopService;
     }
 
     // show list of all products
-
+    // Das ist ein Endpunkt
     @GetMapping("products")
     public List<Product> getAllProducts() {
         return shopService.listProducts();
     }
     // show list of all orders
+    // Das ist ein Endpunkt
     @GetMapping("orders")
     public List<Order> getAllOrders() {
         return shopService.listOrders();
---
+++ src/test/java/com/github/licetrosales/orderdbweblu/service/ShopServiceTest.java
@@ -0,0 +1,38 @@
+package com.github.licetrosales.orderdbweblu.service;
+
+import com.github.licetrosales.orderdbweblu.model.Order;
+import com.github.licetrosales.orderdbweblu.repository.OrderRepository;
+import com.github.licetrosales.orderdbweblu.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ShopServiceTest {
+
+    // Was brauchen wir noch?
+    OrderRepository orderRepository = mock(OrderRepository.class);
+    ProductRepository productRepository = mock(ProductRepository.class);
+    // Was wir wollen testen
+
+    ShopService shopService = new ShopService(orderRepository, productRepository);
+
+    // Methodenname - Was SOLL passieren, wenn EIN Fall eintritt
+    @Test
+    void getOrder_shouldReturnOrder_whenOrderIsValid() {
+
+        //GIVEN
+        String orderId1 = "1";
+        Order expectedOrder = new Order();
+        expectedOrder.setId(orderId1);
+
+        when(orderRepository.get(orderId1)).thenReturn(expectedOrder);
+        //WHEN
+        Order result = shopService.getOrder(orderId1);
+
+        //THEN
+        //Ergebnisse vergleichen - result mit Order mit der Id 1
+        assertEquals(expectedOrder, result);
+    }
+}
